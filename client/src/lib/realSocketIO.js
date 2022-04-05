e8e7cb9a6546fecbd1787a0700d5f42466c12ca7
Naoyuki Sogo <sogo@longtail-software.co.jp>
2022-04-05 21:50:04 +0900

    fix socket IO client

--- client/src/lib/realSocketIO.js
+++ client/src/lib/realSocketIO.js
@@ -6,59 +6,62 @@ let initialized=false;
 let socket = null;
 let uploader = null;
 
-export default {
-  init(auth){
-    socket = auth ? io("/", { transposrts: ["websocket"], auth }) : io("/", { transposrts: ["websocket"] });
-    uploader = new SocketIOFileUpload(socket);
-    uploader.chunkSize = 1024 * 100;
-    initialized=true;
-  },
-  generalCallback: (rt)=>{
-    if(rt instanceof Error){
-      console.log(rt);
-    }
-  },
-  onGlobal: (event, callback)=>{
-    if (! initialized) {
-      init();
-    }
-    socket.on(event, callback);
-  },
-  emitGlobal: (event, ...args)=>{
-    if (! initialized) {
-      init();
-    }
-    socket.emit(event, ...args);
-  },
-  close: ()=>{
-    if (! initialized) {
-      return;
-    }
+const init = (auth)=>{
+  socket = auth ? io("/", { transposrts: ["websocket"], auth }) : io("/", { transposrts: ["websocket"] });
+  uploader = new SocketIOFileUpload(socket);
+  uploader.chunkSize = 1024 * 100;
+  initialized=true;
+};
+const generalCallback = (rt)=>{
+  if(rt instanceof Error){
+    console.log(rt);
+  }
+};
+const initIfNeeded = (auth)=>{
+  if (! initialized) {
+    init(auth);
+  }
+};
+const   onGlobal= (event, callback)=>{
+  initIfNeeded();
+  socket.on(event, callback);
+};
+const  emitGlobal= (event, ...args)=>{
+  initIfNeeded();
+  socket.emit(event, ...args);
+};
+const   close= ()=>{
+  if (socket) {
     socket.close();
     socket = null;
-  },
-  listenOnDrop: (...args)=>{
-    if (! initialized) {
-      init();
-    }
-    uploader.listenOnDrop(...args);
-  },
-  prompt: ()=>{
-    if (! initialized) {
-      init();
-    }
-    uploader.prompt();
-  },
-  onUploaderEvent: (event, callback)=>{
-    if (! initialized) {
-      init();
-    }
-    uploader.addEventListener(event, callback);
-  },
-  removeUploaderEvent: (event, callback)=>{
-    if (! initialized) {
-      return;
-    }
-    uploader.removeEventListener(event, callback);
-  },
+  }
+};
+const  listenOnDrop= (...args)=>{
+  initIfNeeded();
+  uploader.listenOnDrop(...args);
+};
+const  prompt= ()=>{
+  initIfNeeded();
+  uploader.prompt();
+};
+const  onUploaderEvent= (event, callback)=>{
+  initIfNeeded();
+  uploader.addEventListener(event, callback);
+};
+const removeUploaderEvent= (event, callback)=>{
+  initIfNeeded();
+  uploader.removeEventListener(event, callback);
+};
+
+export default {
+  init,
+  generalCallback,
+  initIfNeeded,
+  onGlobal,
+  emitGlobal,
+  close,
+  listenOnDrop,
+  prompt,
+  onUploaderEvent,
+  removeUploaderEvent
 };
