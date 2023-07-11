56108695bd880d6900ba20dd54ebee814e1fc932
Naoyuki Sogo <sogo@longtail-software.co.jp>
2023-07-11 20:42:04 +0900

    update vue,vue-router,vuex,vuetify

--- client/src/home.js
+++ client/src/home.js
@@ -3,13 +3,12 @@
  * Copyright (c) Research Institute for Information Technology(RIIT), Kyushu University. All rights reserved.
  * See License in the project root for the license information.
  */
-import Vue from "vue";
+import {createApp} from "vue";
 import Home from "./components/Home.vue";
 import vuetify from '@/plugins/vuetify'
 
-Vue.config.productionTip = false;
 
-new Vue({
+createApp({
   vuetify,
   render: function (h) { return h(Home); },
 }).$mount("#app");
--- client/src/remotehost.js
+++ client/src/remotehost.js
@@ -3,13 +3,12 @@
  * Copyright (c) Research Institute for Information Technology(RIIT), Kyushu University. All rights reserved.
  * See License in the project root for the license information.
  */
-import Vue from "vue";
+import {createApp} from "vue";
 import Remotehost from "./components/Remotehost.vue";
 import vuetify from '@/plugins/vuetify'
 
-Vue.config.productionTip = false;
 
-new Vue({
+createApp({
   vuetify,
   render: function (h) { return h(Remotehost); },
 }).$mount("#app");
