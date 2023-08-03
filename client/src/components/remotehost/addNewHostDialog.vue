7ab6278573ec256795330bcf79b957530bfa3129
Naoyuki Sogo <sogo@longtail-software.co.jp>
2023-08-04 08:12:09 +0900

    fix v-model

--- client/src/components/remotehost/addNewHostDialog.vue
+++ client/src/components/remotehost/addNewHostDialog.vue
@@ -270,7 +270,7 @@
       fileBrowser
     },
     props: {
-      value: Boolean,
+      modelValue: Boolean,
       title: { type: String, default: "remote host" },
       maxWidth: { type: String, default: "50%" },
       hostNames: {type: Array, default: ()=>{return [];}},
@@ -300,10 +300,10 @@
       },
       openDialog: {
         get () {
-          return this.value;
+          return this.modelValue;
         },
         set (value) {
-          this.$emit("input", value);
+          this.$emit("update:modelValue", value);
         },
       },
     },
