66c3c4a412f3a15b9600f835adae20aef867adf5
Naoyuki Sogo <sogo@longtail-software.co.jp>
2023-11-08 22:17:13 +0900

    - add context menue to component, link, file-link - remove delete button from componentProperty - obsolute project controll APIs

---
+++ client/src/components/componentGraph/contextMenu.vue
@@ -0,0 +1,48 @@
+<template>
+  <g v-for='(menu, index) in items'>
+    <rect :x=x :y=y+index*height
+      @click.stop=onClick(menu.event)
+      :width=width :height=height fill=white :stroke-width=12 stroke="transparent"
+    />
+    <text :x=x :y=y+index*height+height/2
+      @click.stop=onClick(menu.event)
+      fill=black :text-anchor="start" >
+      {{ menu.label }}
+    </text>
+  </g>
+</template>
+<script>
+"use strict";
+
+import { boxWidth, textHeight } from "@/lib/constants.json"
+export default{
+  name:"context-menu",
+  props:{
+    x:{
+      required:true,
+      type: Number
+    },
+    y:{
+      required:true,
+      type: Number
+    },
+    items:{
+      required:true,
+      type: Array
+    }
+  },
+  methods:{
+    onClick(e){
+      console.log("onClick",e);
+      this.$emit(e)
+    },
+  },
+  data(){
+    return {
+      width: boxWidth*0.5,
+      height: textHeight
+    }
+  },
+}
+</script>
+
