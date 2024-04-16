e74266160b6e8fc32ac3872e7288b32c16462032
Naoyuki Sogo <sogo@longtail-software.co.jp>
2024-04-16 11:54:08 +0900

    - fix UT - rewrite release test scripts with docker-compose

--- server/test/release_test_local.sh
+++ server/test/release_test_local.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
-CONFIG_DIR=/tmp/WHEEL_CONFIG_DIR
+export CONFIG_DIR=/tmp/WHEEL_CONFIG_DIR
 TAG_TEST_SERVER=wheel_release_test_server
+
+docker stop ${TAG_TEST_SERVER} 2>/dev/null
 
 echo "prepareing remotehost"
 mkdir ${CONFIG_DIR} 2>/dev/null
-./prepare_remotehost_container.sh ${CONFIG_DIR} ${TAG_TEST_SERVER}
+{
+echo '[{'
+echo '  "name": "testServer",'
+echo '  "host": "localhost",'
+echo '  "path": "/home/testuser",'
+echo '  "keyFile": null,'
+echo '  "username": "testuser",'
+echo '  "numJob": 5,'
+echo '  "port": 4000,'
+echo '  "id": "dummy-id",'
+echo '  "jobScheduler": "PBSPro",'
+echo '  "renewInterval": 0,'
+echo '  "renewDelay": 0,'
+echo '  "statusCheckInterval": 10,'
+echo '  "maxStatusCheckError": 10,'
+echo '  "readyTimeout": 5000'
+echo '}]'
+} > ${CONFIG_DIR}/remotehost.json
+
+echo boot up test server
+docker-compose up ${TAG_TEST_SERVER} -d
+
+echo remove entry from known_hosts
+ssh-keygen -R '[localhost]:4000' 2>/dev/null
+
 
 echo "start UT"
-WHEEL_LOGLEVEL=debug WHEEL_CONFIG_DIR=/tmp/WHEEL_CONFIG_DIR WHEEL_TEST_REMOTEHOST=testServer WHEEL_TEST_REMOTE_PASSWORD=passw0rd npm run test
+WHEEL_CONFIG_DIR=/tmp/WHEEL_CONFIG_DIR WHEEL_TEST_REMOTEHOST=testServer WHEEL_TEST_REMOTE_PASSWORD=passw0rd npm run test
 echo "UT finished"
-docker stop ${TAG_TEST_SERVER}
+
+docker-compose down
