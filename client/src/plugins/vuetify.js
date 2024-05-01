234ecba20e22ba04e2a660e37d3bd72ba9963dc0
Naoyuki Sogo <sogo@longtail-software.co.jp>
2024-05-01 16:58:47 +0900

    fix package include path

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -9,7 +9,6 @@ import { createVuetify } from "vuetify";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import { VDataTable } from "vuetify/labs/VDataTable";
 import colors from "vuetify/lib/util/colors";
 
 const wheelTheme = {
@@ -28,8 +27,7 @@ const wheelTheme = {
 };
 export default createVuetify({
   components: {
-    ...components,
-    VDataTable
+    ...components
   },
   directives,
   theme: {
