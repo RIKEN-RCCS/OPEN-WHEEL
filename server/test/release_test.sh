c6c932bc2e71503a88464344ff9ca7b3ac1d7d07
Naoyuki Sogo <sogo@longtail-software.co.jp>
2024-10-24 16:01:44 +0900

    - update test scripts - fix dispatcher test

--- server/test/release_test.sh
+++ server/test/release_test.sh
@@ -2,18 +2,14 @@
 TEST_DIR=$(cd $(dirname $0);pwd)
 pushd ${TEST_DIR}
 
-TAG=wheel_release_test
+TAG=wheel_release_test #set this image name in compose.yml
 TAG_TEST_SERVER=wheel_release_test_server
 
-#clean up containers just in case
-docker stop ${TAG} 2>/dev/null
-docker stop ${TAG_TEST_SERVER} 2>/dev/null
-docker rm ${TAG} 2>/dev/null
-
+#
 # crate config files
 #
-CONFIG_DIR=$(mktemp -d tmp.XXXXXXXXXX)
-export CONFIG_DIR
+WHEEL_CONFIG_DIR=$(mktemp -d tmp.XXXXXXXXXX)
+export WHEEL_CONFIG_DIR
 
 {
 echo '[{'
@@ -32,7 +28,7 @@ echo '  "statusCheckInterval": 10,'
 echo '  "maxStatusCheckError": 10,'
 echo '  "readyTimeout": 5000'
 echo '}]'
-} > ${CONFIG_DIR}/remotehost.json
+} > ${WHEEL_CONFIG_DIR}/remotehost.json
 
 
 echo boot up test server
@@ -41,18 +37,27 @@ docker compose up ${TAG_TEST_SERVER} -d
 echo remove entry from known_hosts
 ssh-keygen -R 'wheel_release_test_server'
 
-docker compose run --build --rm ${TAG}
+if [ x$1 == x-d ];then
+  export WHEEL_KEEP_FILES_AFTER_LAST_TEST=1
+fi
+
+docker compose run -e WHEEL_KEEP_FILES_AFTER_LAST_TEST --build ${TAG}
 rt=$?
 
-docker compose down
+CONTAINER_NAME=$(docker ps -a --filter "ancestor=${TAG}" --format "{{.Names}}")
 
 #get log files from container
 if [ x$1 == x-c ];then
   LOG_DIR=$(dirname ${TEST_DIR})/$(date "+%Y%m%d-%H%M")
   mkdir $LOG_DIR
-  docker cp ${TAG}:/usr/src/server/coverage $LOG_DIR
+  docker cp ${CONTAINER_NAME}:/usr/src/server/coverage $LOG_DIR
 fi
 
-rm -fr ${CONFIG_DIR}
+if [ -n ${WHEEL_KEEP_FILES_AFTER_LAST_TEST} ];then
+  echo copy files after last test to ${TEST_DIR}
+  docker cp ${CONTAINER_NAME}:/usr/src/server/WHEEL_TEST_TMP ${TEST_DIR}/
+fi
 
+docker compose down
+rm -fr ${WHEEL_CONFIG_DIR}
 exit ${rt}
