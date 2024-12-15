1a9d37c300b70562a6dcac64efc5a22b4c9e41b3
Naoyuki Sogo <sogo@longtail-software.co.jp>
2024-12-16 02:46:45 +0900

    - add exportProject() and it's unit test - add gitClone() and gitArchive()

---
+++ server/app/core/exportProject.js
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) Center for Computational Science, RIKEN All rights reserved.
+ * Copyright (c) Research Institute for Information Technology(RIIT), Kyushu University. All rights reserved.
+ * See License in the project root for the license information.
+ */
+"use strict";
+const path = require("path");
+const fs = require("fs-extra");
+const { createTempd } = require("./tempd.js");
+const { readJsonGreedy } = require("./fileUtils.js");
+const { projectJsonFilename } = require("../db/db.js");
+const { gitAdd, gitClone, gitArchive, gitCommit } = require("./gitOperator2.js");
+const { setComponentStateR } = require("./projectFilesOperator.js");
+
+/**
+ * export existing project as archive file
+ * @param {string} projectRootDir - project's root path
+ * @param {string} name - user name
+ * @param {string} mail - user's e-mail address
+ * @param {string} memo - memo to exported archive
+ * @returns {string} - download URL
+ */
+async function exportProject(projectRootDir, name = null, mail = null, memo = null) {
+  const { dir } = await createTempd(projectRootDir, "exportProject");
+  const workDir = await fs.mkdtemp(`${dir}/`);
+  await gitClone(workDir, 1, projectRootDir);
+  const tmpProjectRootDir = path.join(workDir, path.basename(projectRootDir));
+  await setComponentStateR(tmpProjectRootDir, tmpProjectRootDir, "not-started");
+
+  const filename = path.resolve(tmpProjectRootDir, projectJsonFilename);
+  const projectJson = await readJsonGreedy(filename);
+  if (!projectJson.exporter) {
+    projectJson.exporter = {};
+  }
+
+  if (typeof name === "string") {
+    projectJson.exporter.name = name;
+  }
+  if (typeof mail === "string") {
+    projectJson.exporter.mail = mail;
+  }
+  if (typeof memo === "string") {
+    projectJson.exporter.memo = memo;
+  }
+
+  projectJson.state = "not-started";
+
+  const locale = Intl.DateTimeFormat().resolvedOptions().locale;
+  const exportDate = new Intl.DateTimeFormat(locale, { dateStyle: "medium", timeStyle: "long" }).format();
+  projectJson.exportDate = exportDate;
+
+  await fs.writeJson(filename, projectJson, { spaces: 4 });
+
+  await gitAdd(tmpProjectRootDir, filename);
+  await gitCommit(tmpProjectRootDir, "export project");
+
+  const archiveFilename = path.join(dir, `WHEEL_project_${projectJson.name}.tgz`);
+  await gitArchive(tmpProjectRootDir, archiveFilename, projectRootDir);
+
+  const baseURL = process.env.WHEEL_BASE_URL || "";
+  const url = `${baseURL}/${path.join(path.relative(path.dirname(dir), archiveFilename))}`;
+  return url;
+}
+
+module.exports = {
+  exportProject
+};
---
+++ server/test/exportProject.js
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) Center for Computational Science, RIKEN All rights reserved.
+ * Copyright (c) Research Institute for Information Technology(RIIT), Kyushu University. All rights reserved.
+ * See License in the project root for the license information.
+ */
+"use strict";
+const util = require("node:util");
+const exec = util.promisify(require("node:child_process").exec);
+const path = require("node:path");
+const fs = require("fs-extra");
+
+//setup test framework
+const chai = require("chai");
+const expect = chai.expect;
+chai.use(require("chai-fs"));
+chai.use(require("chai-as-promised"));
+
+//helper
+const { createNewComponent, createNewProject } = require("../app/core/projectFilesOperator");
+const { gitCommit } = require("../app/core/gitOperator2.js");
+const { projectJsonFilename, componentJsonFilename } = require("../app/db/db.js");
+
+//testee
+const { exportProject } = require("../app/core/exportProject.js");
+
+//test data
+const testDirRoot = path.resolve("./", "WHEEL_TEST_TMP");
+const projectRootDir = path.resolve(testDirRoot, "test_project.wheel");
+
+describe("#export project", function () {
+  this.timeout(10000);
+  const projectName = "test_project";
+  const extractDir = path.resolve(testDirRoot, "tmp");
+  let workflow0;
+  beforeEach(async ()=>{
+    await fs.remove(testDirRoot);
+    await createNewProject(projectRootDir, projectName, null, "test", "test@example.com");
+    await createNewComponent(projectRootDir, projectRootDir, "task", { x: 10, y: 10 });
+    await createNewComponent(projectRootDir, projectRootDir, "task", { x: 10, y: 10 });
+    await createNewComponent(projectRootDir, projectRootDir, "task", { x: 10, y: 10 });
+    workflow0 = await createNewComponent(projectRootDir, projectRootDir, "workflow", { x: 10, y: 10 });
+    await createNewComponent(projectRootDir, path.resolve(projectRootDir, workflow0.name), "task", { x: 10, y: 10 });
+    await createNewComponent(projectRootDir, path.resolve(projectRootDir, workflow0.name), "task", { x: 10, y: 10 });
+    await createNewComponent(projectRootDir, path.resolve(projectRootDir, workflow0.name), "task", { x: 10, y: 10 });
+    await gitCommit(projectRootDir);
+  });
+  after(async ()=>{
+    if (!process.env.WHEEL_KEEP_FILES_AFTER_LAST_TEST) {
+      await fs.remove(testDirRoot);
+    }
+  });
+  it("should export project as tar.gz", async ()=>{
+    expect(await exportProject(projectRootDir)).to.be.a("string").and.match(new RegExp(`WHEEL_project_${projectName}.tgz`));
+  });
+  it("should export project even if not-committed files exist and exclude them", async ()=>{
+    await fs.outputFile(path.resolve(projectRootDir, workflow0.name, "hoge"), "hoge");
+    const url = await exportProject(projectRootDir);
+    expect(url).to.be.a("string").and.match(new RegExp(`WHEEL_project_${projectName}.tgz`));
+    await fs.ensureDir(extractDir);
+    await exec(`tar xfz ${path.join("/tmp/exportProject", url)} -C ${extractDir}`);
+    expect(path.join(extractDir, workflow0.name, "hoge")).to.not.be.a.path();
+    expect(path.join(extractDir, projectJsonFilename)).to.be.a.file();
+  });
+  it("should export project and status changed to 'not-started'", async ()=>{
+    const workflowJson = await fs.readJson(path.join(projectRootDir, workflow0.name, componentJsonFilename));
+    workflowJson.state = "hoge";
+    await fs.writeJson(path.join(projectRootDir, workflow0.name, componentJsonFilename), workflowJson);
+
+    const projectJson = await fs.readJson(path.join(projectRootDir, projectJsonFilename));
+    projectJson.state = "huga";
+    await fs.writeJson(path.join(projectRootDir, projectJsonFilename), projectJson);
+    await exec(`cd ${projectRootDir}&&git add -u&& git commit -m 'test'`);
+
+    const url = await exportProject(projectRootDir);
+    expect(url).to.be.a("string").and.match(new RegExp(`WHEEL_project_${projectName}.tgz`));
+    await fs.ensureDir(extractDir);
+    await exec(`tar xfz ${path.join("/tmp/exportProject", url)} -C ${extractDir}`);
+
+    const workflowJsonAfter = await fs.readJson(path.join(extractDir, workflow0.name, componentJsonFilename));
+    expect(workflowJsonAfter.state).to.be.a("string").and.equal("not-started");
+    const projectJsonAfter = await fs.readJson(path.join(extractDir, projectJsonFilename));
+    expect(projectJsonAfter.state).to.be.a("string").and.equal("not-started");
+  });
+});
