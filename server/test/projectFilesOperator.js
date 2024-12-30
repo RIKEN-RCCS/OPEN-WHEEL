2364f00340756ff5bd3deaa165adbfff3bd7e88f
KEISUKE SARUWATARI <86877155+atomic-nodak@users.noreply.github.com>
2024-12-31 00:27:33 +0900

    create projectFilesOperator#removeTrailingPathSep test code (#246)

--- server/test/projectFilesOperator.js
+++ server/test/projectFilesOperator.js
@@ -7,6 +7,7 @@
 const { expect } = require("chai");
 const { describe, it } = require("mocha");
 const rewire = require("rewire");
+const path = require("path");
 
 describe("#isSurrounded", ()=>{
   let projectFilesOperator;
@@ -140,5 +141,51 @@ describe("#glob2Array", ()=>{
     const input = "{{file1,file2},file3}";
     const expectedOutput = ["file1", "file2}", "file3"];
     expect(glob2Array(input)).to.deep.equal(expectedOutput);
+  });
+});
+
+describe("#removeTrailingPathSep", ()=>{
+  let projectFilesOperator;
+  let removeTrailingPathSep;
+
+  beforeEach(()=>{
+    projectFilesOperator = rewire("../app/core/projectFilesOperator.js");
+    removeTrailingPathSep = projectFilesOperator.__get__("removeTrailingPathSep");
+  });
+
+  it("should remove trailing path separator for POSIX paths", ()=>{
+    const input = "/path/to/directory/";
+    const expected = path.sep === "/" ? "/path/to/directory" : input; //実行環境がPOSIXなら削除、Windowsならそのまま
+    expect(removeTrailingPathSep(input)).to.equal(expected);
+  });
+
+  it("should remove trailing path separator for Windows paths", ()=>{
+    const input = "C:\\path\\to\\directory\\";
+    const expected = path.sep === "\\" ? "C:\\path\\to\\directory" : input; //Windowsなら削除、POSIXならそのまま
+    expect(removeTrailingPathSep(input)).to.equal(expected);
+  });
+
+  it("should not alter a path without trailing path separator", ()=>{
+    const input = "/path/to/directory";
+    const expected = "/path/to/directory";
+    expect(removeTrailingPathSep(input)).to.equal(expected);
+  });
+
+  it("should handle an empty string gracefully", ()=>{
+    const input = "";
+    const expected = "";
+    expect(removeTrailingPathSep(input)).to.equal(expected);
+  });
+
+  it("should handle paths consisting of only a single path separator", ()=>{
+    const input = path.sep;
+    const expected = "";
+    expect(removeTrailingPathSep(input)).to.equal(expected);
+  });
+
+  it("should recursively remove multiple trailing path separators", ()=>{
+    const input = `/path/to/directory///`.replace(/\//g, path.sep); //セパレータを現在の実行環境に合わせる
+    const expected = `/path/to/directory`.replace(/\//g, path.sep); //入力と同様に変換後の期待値
+    expect(removeTrailingPathSep(input)).to.equal(expected);
   });
 });
