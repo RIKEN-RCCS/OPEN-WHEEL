6d253ce08efaaf52dc82ab79da139e19df72011c
KAMIJIMA Ryoya <ryoya.second@gmail.com>
2025-01-16 15:41:33 +0900

    create dispatchUtils#isFinishedState test code

---
+++ server/test/app/core/dispatchUtils.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) Center for Computational Science, RIKEN All rights reserved.
+ * Copyright (c) Research Institute for Information Technology(RIIT), Kyushu University. All rights reserved.
+ * See License in the project root for the license information.
+ */
+"use strict";
+const { expect } = require("chai");
+const { describe, it } = require("mocha");
+const { isFinishedState } = require("../../../app/core/dispatchUtils");
+
+describe("#isFinishedState", ()=>{
+  it("should return true if the status is finished", ()=>{
+    expect(isFinishedState("finished")).to.be.true;
+  });
+
+  it("should return true if the status is failed", ()=>{
+    expect(isFinishedState("failed")).to.be.true;
+  });
+
+  it("should return true if the status is unknown", ()=>{
+    expect(isFinishedState("unknown")).to.be.true;
+  });
+
+  it("should return false if the status is not finished, failed or unkown", ()=>{
+    expect(isFinishedState("processing")).to.be.false;
+  });
+});
