8c0fca2514d6fae2181ca9c0de45a44cb2b6f706
KEISUKE SARUWATARI <86877155+atomic-nodak@users.noreply.github.com>
2025-01-19 16:20:27 +0900

    create projectFilesOperator#addFileLinkToParent test code (#276)

--- server/test/app/core/projectFilesOperator.js
+++ server/test/app/core/projectFilesOperator.js
@@ -2585,3 +2585,90 @@ describe("#removeAllLinkFromComponent", ()=>{
     expect(writeComponentJsonByIDMock.calledWith(projectRootDir, "dstComponent2", sinon.match.object)).to.be.true;
   });
 });
+
+describe("#addFileLinkToParent", ()=>{
+  let rewireProjectFilesOperator;
+  let addFileLinkToParent;
+  let getComponentDirMock, readComponentJsonMock, writeComponentJsonMock;
+
+  beforeEach(()=>{
+    rewireProjectFilesOperator = rewire("../../../app/core/projectFilesOperator.js");
+    addFileLinkToParent = rewireProjectFilesOperator.__get__("addFileLinkToParent");
+
+    getComponentDirMock = sinon.stub();
+    readComponentJsonMock = sinon.stub();
+    writeComponentJsonMock = sinon.stub();
+
+    rewireProjectFilesOperator.__set__({
+      getComponentDir: getComponentDirMock,
+      readComponentJson: readComponentJsonMock,
+      writeComponentJson: writeComponentJsonMock
+    });
+  });
+
+  afterEach(()=>{
+    sinon.restore();
+  });
+
+  it("should add a file link to parent component correctly", async ()=>{
+    const projectRootDir = "/mock/project/root";
+    const srcNode = "srcNode1";
+    const srcName = "outputFile1";
+    const dstName = "inputFile1";
+
+    const srcDir = "/mock/project/root/src";
+    const parentDir = "/mock/project/root";
+    const srcJson = {
+      ID: "srcNode1",
+      outputFiles: [{ name: "outputFile1", dst: [] }]
+    };
+    const parentJson = {
+      ID: "parentNode1",
+      outputFiles: [{ name: "inputFile1" }]
+    };
+
+    getComponentDirMock.onFirstCall().resolves(srcDir);
+    readComponentJsonMock.withArgs(srcDir).resolves(srcJson);
+    readComponentJsonMock.withArgs(parentDir).resolves(parentJson);
+    writeComponentJsonMock.resolves();
+
+    await addFileLinkToParent(projectRootDir, srcNode, srcName, dstName);
+
+    expect(srcJson.outputFiles[0].dst).to.deep.include({
+      dstNode: "parentNode1",
+      dstName: "inputFile1"
+    });
+    expect(parentJson.outputFiles[0].origin).to.deep.include({
+      srcNode: "srcNode1",
+      srcName: "outputFile1"
+    });
+
+    expect(writeComponentJsonMock.calledTwice).to.be.true;
+    expect(writeComponentJsonMock.firstCall.args).to.deep.equal([
+      projectRootDir,
+      srcDir,
+      srcJson
+    ]);
+    expect(writeComponentJsonMock.secondCall.args).to.deep.equal([
+      projectRootDir,
+      parentDir,
+      parentJson
+    ]);
+  });
+
+  it("should throw an error if srcNode does not exist", async ()=>{
+    const projectRootDir = "/mock/project/root";
+    const srcNode = "invalidNode";
+    const srcName = "outputFile1";
+    const dstName = "inputFile1";
+
+    getComponentDirMock.rejects(new Error("srcNode not found"));
+
+    try {
+      await addFileLinkToParent(projectRootDir, srcNode, srcName, dstName);
+      throw new Error("Expected addFileLinkToParent to throw");
+    } catch (err) {
+      expect(err.message).to.equal("srcNode not found");
+    }
+  });
+});
