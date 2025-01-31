11a7b7da7ea2e08b5872cf35e518ab5d9cbde7c6
KEISUKE SARUWATARI <86877155+atomic-nodak@users.noreply.github.com>
2025-01-31 21:24:49 +0900

    create projectFilesOperator#getChildren test code (#286)

--- server/test/app/core/projectFilesOperator.js
+++ server/test/app/core/projectFilesOperator.js
@@ -3302,3 +3302,63 @@ describe("#makeDir", ()=>{
     expect(fsMock.mkdir.calledOnceWithExactly("/mock/path0")).to.be.true;
   });
 });
+
+describe("#getChildren", ()=>{
+  let rewireProjectFilesOperator;
+  let getChildren;
+  let getComponentDirMock;
+  let readJsonGreedyMock;
+  let globMock;
+
+  beforeEach(()=>{
+    rewireProjectFilesOperator = rewire("../../../app/core/projectFilesOperator.js");
+    getChildren = rewireProjectFilesOperator.__get__("getChildren");
+
+    getComponentDirMock = sinon.stub();
+    readJsonGreedyMock = sinon.stub();
+    globMock = sinon.stub();
+    rewireProjectFilesOperator.__set__({
+      getComponentDir: getComponentDirMock,
+      readJsonGreedy: readJsonGreedyMock,
+      promisify: ()=>globMock
+    });
+  });
+
+  it("should return an empty array if the directory is not found", async ()=>{
+    getComponentDirMock.resolves(null);
+
+    const result = await getChildren("/mock/project", "invalidID", false);
+
+    expect(result).to.deep.equal([]);
+  });
+
+  it("should return an empty array if no child components are found", async ()=>{
+    getComponentDirMock.resolves("/mock/project/component");
+    globMock.resolves([]);
+
+    const result = await getChildren("/mock/project", "validID", false);
+
+    expect(result).to.deep.equal([]);
+  });
+
+  it("should return an array of child components excluding subComponents", async ()=>{
+    getComponentDirMock.resolves("/mock/project/component");
+    globMock.resolves(["/mock/project/component/child1/cmp.wheel.json", "/mock/project/component/child2/cmp.wheel.json"]);
+
+    readJsonGreedyMock.withArgs("/mock/project/component/child1/cmp.wheel.json").resolves({ ID: "child1", subComponent: false });
+    readJsonGreedyMock.withArgs("/mock/project/component/child2/cmp.wheel.json").resolves({ ID: "child2", subComponent: true });
+
+    const result = await getChildren("/mock/project", "validID", false);
+
+    expect(result).to.deep.equal([{ ID: "child1", subComponent: false }]);
+  });
+
+  it("should handle the case where parentID is a directory path", async ()=>{
+    globMock.resolves(["/mock/project/parent/child/cmp.wheel.json"]);
+    readJsonGreedyMock.resolves({ ID: "child", subComponent: false });
+
+    const result = await getChildren("/mock/project", "/mock/project/parent", true);
+
+    expect(result).to.deep.equal([{ ID: "child", subComponent: false }]);
+  });
+});
