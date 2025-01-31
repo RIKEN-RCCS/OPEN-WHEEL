ec0054a4cd5171212d2ad288f24558e412f9b8d0
KEISUKE SARUWATARI <86877155+atomic-nodak@users.noreply.github.com>
2025-01-31 21:34:59 +0900

    create projectFilesOperator#checkRemoteStoragePathWritePermission test code (#287)

--- server/test/app/core/projectFilesOperator.js
+++ server/test/app/core/projectFilesOperator.js
@@ -3362,3 +3362,64 @@ describe("#getChildren", ()=>{
     expect(result).to.deep.equal([{ ID: "child", subComponent: false }]);
   });
 });
+
+describe("#checkRemoteStoragePathWritePermission", ()=>{
+  let rewireProjectFilesOperator;
+  let checkRemoteStoragePathWritePermission;
+  let getSshMock;
+  let remoteHostMock;
+  let sshExecMock;
+
+  beforeEach(()=>{
+    rewireProjectFilesOperator = rewire("../../../app/core/projectFilesOperator.js");
+    checkRemoteStoragePathWritePermission = rewireProjectFilesOperator.__get__("checkRemoteStoragePathWritePermission");
+
+    remoteHostMock = {
+      getID: sinon.stub()
+    };
+
+    sshExecMock = sinon.stub();
+    getSshMock = sinon.stub().returns({ exec: sshExecMock });
+    rewireProjectFilesOperator.__set__({
+      getSsh: getSshMock,
+      remoteHost: remoteHostMock
+    });
+  });
+
+  afterEach(()=>{
+    sinon.restore();
+  });
+
+  it("should resolve when the storage path has write permission", async ()=>{
+    const projectRootDir = "/mock/project/root";
+    const params = { host: "remoteHost1", storagePath: "/remote/path" };
+
+    remoteHostMock.getID.withArgs("name", "remoteHost1").returns("host123");
+    sshExecMock.withArgs("test -w /remote/path").returns(0);
+
+    await expect(checkRemoteStoragePathWritePermission(projectRootDir, params)).to.be.fulfilled;
+    expect(remoteHostMock.getID.calledOnceWithExactly("name", "remoteHost1")).to.be.true;
+    expect(getSshMock.calledOnceWithExactly(projectRootDir, "host123")).to.be.true;
+    expect(sshExecMock.calledOnceWithExactly("test -w /remote/path")).to.be.true;
+  });
+
+  it("should throw an error when the storage path does not have write permission", async ()=>{
+    const projectRootDir = "/mock/project/root";
+    const params = { host: "remoteHost1", storagePath: "/remote/path" };
+
+    remoteHostMock.getID.withArgs("name", "remoteHost1").returns("host123");
+    sshExecMock.withArgs("test -w /remote/path").returns(1);
+
+    await expect(checkRemoteStoragePathWritePermission(projectRootDir, params)).to.be.rejectedWith("bad permission");
+  });
+
+  it("should throw an error when SSH instance is not available", async ()=>{
+    const projectRootDir = "/mock/project/root";
+    const params = { host: "remoteHost1", storagePath: "/remote/path" };
+
+    remoteHostMock.getID.withArgs("name", "remoteHost1").returns("host123");
+    getSshMock.throws(new Error("ssh instance is not registerd for the project"));
+
+    await expect(checkRemoteStoragePathWritePermission(projectRootDir, params)).to.be.rejectedWith("ssh instance is not registerd for the project");
+  });
+});
