d96a4fc2231436882a62b846774818f525749c26
akasakaryob102@gmail.com <moeryo19921001>
2025-03-04 18:19:29 +0900

    issue #17 isValidName

--- server/test/app/common/regexp.cjs
+++ server/test/app/common/regexp.cjs
@@ -11,6 +11,7 @@ const { expect } = require("chai");
 //testee
 const { isValidOutputFilename } = require("../../../../common/regexp.cjs");
 const { isValidInputFilename } = require("../../../../common/regexp.cjs");
+const { isValidName } = require("../../../../common/regexp.cjs");
 
 describe("UT for regexp class", () => {
   describe("#isValidOutputFilename", () => {
@@ -89,5 +90,95 @@ describe("UT for regexp class", () => {
     it("should return true for filenames with nested paths", () => {
         expect(isValidInputFilename("folder/subfolder/file.txt")).to.be.true;
     });
-});
+  });
+  describe("#isValidOutputFilename", () => {
+    it("should return false for empty string", () => {
+        expect(isValidOutputFilename("")).to.be.false;
+    });
+    it("should return false for only whitespace", () => {
+        expect(isValidOutputFilename("   ")).to.be.false;
+    });
+    it("should return false for reserved names in Windows", () => {
+        expect(isValidOutputFilename("CON.txt")).to.be.false;
+        expect(isValidOutputFilename("PRN.txt")).to.be.false;
+        expect(isValidOutputFilename("AUX.txt")).to.be.false;
+    });
+    it("should return false for invalid characters", () => {
+        expect(isValidOutputFilename("invalid|name.txt")).to.be.false;
+        expect(isValidOutputFilename("invalid<name>.txt")).to.be.false;
+    });
+    it("should return true for valid filenames", () => {
+        expect(isValidOutputFilename("valid_name.txt")).to.be.true;
+        expect(isValidOutputFilename("valid-name_123.txt")).to.be.true;
+        expect(isValidOutputFilename("valid.name.txt")).to.be.true;
+    });
+    it("should return true for valid filenames with path separators", () => {
+        expect(isValidOutputFilename("dir/valid_name.txt")).to.be.true;
+        expect(isValidOutputFilename("dir\\valid_name.txt")).to.be.true;
+    });
+    it("should return false for invalid path separators", () => {
+        expect(isValidOutputFilename("dir\\invalid|name.txt")).to.be.false;
+    });
+    it("should return true for filenames with {{ and }}", () => {
+        expect(isValidOutputFilename("file_{{var}}.txt")).to.be.true;
+    });
+    it("should return false for filenames with forbidden characters after removing {{ }}", () => {
+        expect(isValidOutputFilename("file_{{var}}|.txt")).to.be.false;
+    });
+    it("should return true for filenames with dots and extensions", () => {
+        expect(isValidOutputFilename("file.name.with.dots.txt")).to.be.true;
+    });
+    it("should return true for filenames with nested paths", () => {
+        expect(isValidOutputFilename("folder/subfolder/file.txt")).to.be.true;
+    });
+    it("should allow meta characters in filenames", () => {
+        expect(isValidOutputFilename("file*?.txt")).to.be.true;
+        expect(isValidOutputFilename("file[1-3].txt")).to.be.true;
+    });
+  });
+  describe("#isValidName", () => {
+    it("should return false for empty string", () => {
+        expect(isValidName("")).to.be.false;
+    });
+    it("should return false for only whitespace", () => {
+        expect(isValidName("   ")).to.be.false;
+    });
+    it("should return true for reserved names in Windows(dosen't have suffix)", () => {
+        expect(isValidName("CON")).to.be.true;
+        expect(isValidName("PRN")).to.be.true;
+        expect(isValidName("AUX")).to.be.true;
+    });
+    it("should return false for names with invalid characters", () => {
+        expect(isValidName("invalid|name")).to.be.false;
+        expect(isValidName("invalid<name>")).to.be.false;
+        expect(isValidName("invalid/name")).to.be.false;
+        expect(isValidName("invalid\\name")).to.be.false;
+    });
+    it("should return true for valid alphanumeric names", () => {
+        expect(isValidName("validName")).to.be.true;
+        expect(isValidName("valid123")).to.be.true;
+    });
+    it("should return true for names with underscores and hyphens", () => {
+        expect(isValidName("valid_name")).to.be.true;
+        expect(isValidName("valid-name")).to.be.true;
+    });
+    it("should return false for names with spaces", () => {
+        expect(isValidName("invalid name")).to.be.false;
+    });
+    it("should return false for names starting with invalid characters", () => {
+        expect(isValidName("-invalid")).to.be.true;
+        expect(isValidName("_invalid")).to.be.true;
+    });
+    it("should return false for names ending with invalid characters", () => {
+        expect(isValidName("invalid-")).to.be.true;
+        expect(isValidName("invalid_")).to.be.true;
+    });
+    it("should return false for names containing dots", () => {
+        expect(isValidName("invalid.name")).to.be.false;
+    });
+    it("should return false for names containing special characters", () => {
+        expect(isValidName("name@domain")).to.be.false;
+        expect(isValidName("name#domain")).to.be.false;
+    });
+  });
 });
