35134c7fd80af0fb4246fd6ad24904ff1a931317
Naoyuki Sogo <sogo@longtail-software.co.jp>
2025-08-09 02:34:40 +0900

    improve stability

--- test/cypress/e2e/home.cy.js
+++ test/cypress/e2e/home.cy.js
@@ -14,6 +14,7 @@ describe("02:ホーム画面基本動作確認", ()=>{
    */
   it("02-01-001:構成要素の機能動作確認-ボタン押下時の確認-OPENボタン押下-ワークフロー画面が表示されることを確認", ()=>{
     cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
+    cy.waitProjectList();
     cy.get("[type=\"checkbox\"]").eq(1)
       .check();
     cy.get("[data-cy=\"home-open-btn\"]").click();
@@ -40,6 +41,7 @@ describe("02:ホーム画面基本動作確認", ()=>{
   it("02-01-005:構成要素の機能動作確認-ボタン押下時の確認-BATCH MODE押下-複数プロジェクトが選択可能であることを確認", ()=>{
     cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
     cy.createProject("TestProject2", "TestDescription2");
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-batch_mode-btn\"]").click();
     cy.get("[type=\"checkbox\"]").as("projectCheckbox");
     cy.get("@projectCheckbox").eq(2)
@@ -69,6 +71,7 @@ describe("02:ホーム画面基本動作確認", ()=>{
    */
   it("02-01-007:構成要素の機能動作確認-プロジェクト名押下-プロジェクト名変更ダイアログが表示されることを確認", ()=>{
     cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-project_name-btn\"]").click();
     cy.get("[data-cy=\"home-project_rename-text_field\"]").should("be.visible");
     cy.removeAllProjects();
@@ -82,11 +85,15 @@ describe("02:ホーム画面基本動作確認", ()=>{
    */
   it("02-01-008:機能利用フローの確認-新規プロジェクトの作成-プロジェクトデータ作成場所選択-ディレクトリツリーでプロジェクトデータ作成場所が選択できることを確認", ()=>{
     cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-new-btn\"]").click();
-    cy.wait(1000);
-    cy.get("[data-cy=\"home-file_browser-file_browser\"]").contains(PROJECT_NAME + EXTENSION)
+    cy.get("[data-cy=\"home-project_list-data_table\"]", { timeout: 5000 })
+      .should("contain.text", PROJECT_NAME);
+    cy.get("[data-cy=\"home-file_browser-file_browser\"]")
+      .contains(PROJECT_NAME + EXTENSION)
       .click();
-    cy.get("[data-cy=\"home-file_browser-file_browser\"]").contains("prj.wheel.json")
+    cy.get("[data-cy=\"home-file_browser-file_browser\"]")
+      .contains("prj.wheel.json")
       .should("exist");
     cy.removeAllProjects();
   });
@@ -125,6 +132,7 @@ describe("02:ホーム画面基本動作確認", ()=>{
    */
   it("02-01-011:機能利用フローの確認-新規プロジェクトの作成-createボタン押下-プロジェクトが作成できていることを確認", ()=>{
     cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-project_name-btn\"]").contains(PROJECT_NAME)
       .should("exist");
     cy.get("[data-cy=\"home-project_description-btn\"]").contains(PROJECT_DESCRIPTION)
@@ -163,6 +171,7 @@ describe("02:ホーム画面基本動作確認", ()=>{
    */
   it("02-01-013:機能利用フローの確認-プロジェクト名変更-プロジェクト名押下-プロジェクト名変更ダイアログが表示されることを確認", ()=>{
     cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-project_name-btn\"]").contains(PROJECT_NAME)
       .click();
     cy.get("[data-cy=\"home-project_rename-text_field\"]").should("not.be.visible");
@@ -177,6 +186,7 @@ describe("02:ホーム画面基本動作確認", ()=>{
    */
   it("02-01-014:機能利用フローの確認-プロジェクト名変更-プロジェクト名押下-プロジェクト名が入力できることを確認", ()=>{
     cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-project_name-btn\"]").contains(PROJECT_NAME)
       .click();
     cy.get("[data-cy=\"home-project_rename-text_field\"]").clear();
@@ -194,12 +204,14 @@ describe("02:ホーム画面基本動作確認", ()=>{
    */
   it("02-01-015:機能利用フローの確認-プロジェクト名変更-プロジェクト名押下-変更後のプロジェクト名で一覧に表示されていることを確認", ()=>{
     cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-project_name-btn\"]").contains(PROJECT_NAME)
       .click();
     cy.get("[data-cy=\"home-project_rename-text_field\"]").clear();
-    cy.get("[data-cy=\"home-project_rename-text_field\"]").type("changeProjectName {enter}");
-    cy.get("[data-cy=\"home-project_name-btn\"]").contains("changeProjectName")
-      .should("exist");
+    cy.get("[data-cy=\"home-project_rename-text_field\"]").type("changeProjectName");
+    cy.get("[data-cy=\"home-project_rename-text_field\"]").type("{enter}");
+    cy.get("[data-cy=\"home-project_name-btn\"]").contains("changeProjectName");
+
     cy.removeAllProjects();
   });
 
@@ -211,6 +223,7 @@ describe("02:ホーム画面基本動作確認", ()=>{
    */
   it("02-01-016:機能利用フローの確認-プロジェクト名変更-プロジェクト説明押下-プロジェクト説明変更ダイアログが表示されることを確認", ()=>{
     cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-project_description-btn\"]").contains(PROJECT_DESCRIPTION)
       .click();
     cy.get("[data-cy=\"home-description_change-textarea\"]").should("not.be.visible");
@@ -225,6 +238,7 @@ describe("02:ホーム画面基本動作確認", ()=>{
    */
   it("02-01-017:機能利用フローの確認-プロジェクト名変更-プロジェクト説明押下-プロジェクト説明が入力できることを確認", ()=>{
     cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-project_description-btn\"]").contains(PROJECT_DESCRIPTION)
       .click();
     cy.get("[data-cy=\"home-description_change-textarea\"]").clear();
@@ -242,10 +256,12 @@ describe("02:ホーム画面基本動作確認", ()=>{
    */
   it("02-01-018:機能利用フローの確認-プロジェクト名変更-プロジェクト説明押下-変更後のプロジェクト説明で一覧に表示されていることを確認", ()=>{
     cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-project_description-btn\"]").contains(PROJECT_DESCRIPTION)
       .click();
     cy.get("[data-cy=\"home-description_change-textarea\"]").clear();
     cy.get("[data-cy=\"home-description_change-textarea\"]").type("change-" + PROJECT_DESCRIPTION);
+    cy.get("[data-cy=\"home-description_change-textarea\"]").type("{enter}");
     cy.get("[data-cy=\"home-project_description-btn\"]").contains("change-" + PROJECT_DESCRIPTION)
       .should("exist");
     cy.removeAllProjects();
@@ -259,7 +275,7 @@ describe("02:ホーム画面基本動作確認", ()=>{
    */
   it("02-01-019:機能利用フローの確認-プロジェクトを削除-REMOVEボタン押下-一覧にプロジェクトが表示されていない且つ、実体ファイルが削除されていることを確認", ()=>{
     cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
-    cy.wait(500);
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-project_list-data_table\"]").find("[type=\"checkbox\"]")
       .first()
       .check();
@@ -281,6 +297,7 @@ describe("02:ホーム画面基本動作確認", ()=>{
     const PROJECT_NAME_RANDOM = Math.random().toString(32)
       .substring(2);
     cy.createProject(PROJECT_NAME_RANDOM, PROJECT_DESCRIPTION);
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-project_list-data_table\"]").find("[type=\"checkbox\"]")
       .first()
       .check();
@@ -301,7 +318,7 @@ describe("02:ホーム画面基本動作確認", ()=>{
    */
   it("02-01-021:機能利用フローの確認-プロジェクトを削除-REMOVEボタン押下-BATCH MODE選択-一覧にプロジェクトが表示されていない且つ、実体ファイルが削除されていることを確認", ()=>{
     cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
-    cy.wait(500);
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-batch_mode-btn\"]").click();
     cy.get("[data-cy=\"home-project_list-data_table\"]").find("[type=\"checkbox\"]")
       .first()
@@ -326,6 +343,7 @@ describe("02:ホーム画面基本動作確認", ()=>{
     const PROJECT_NAME_RANDOM = Math.random().toString(32)
       .substring(2);
     cy.createProject(PROJECT_NAME_RANDOM, PROJECT_DESCRIPTION);
+    cy.waitProjectList();
     cy.get("[data-cy=\"home-batch_mode-btn\"]").click();
     cy.get("[data-cy=\"home-project_list-data_table\"]").find("[type=\"checkbox\"]")
       .first()
