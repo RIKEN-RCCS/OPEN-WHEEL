94d271722578e207832d1585a436b210b31b8ff5
Naoyuki Sogo <sogo@longtail-software.co.jp>
2025-08-12 13:00:53 +0900

    add reproduction test of #949

--- test/cypress/e2e/sourceAndViewer.cy.js
+++ test/cypress/e2e/sourceAndViewer.cy.js
@@ -38,4 +38,24 @@ describe("05:Source&Viewerコンポーネント機能正常動作確認", ()=>{
     cy.checkProjectStatus("finished", 10000);
     cy.contains("open viewer screen").should("exist");
   });
+  it("should not open old viewer component's result after re-creating project with same name (reproduction test of #948)", ()=>{
+    cy.createComponent(DEF_COMPONENT_IF, IF_NAME_0, 300, 500);
+    cy.createDirOrFile(TYPE_FILE, "test-a", true);
+    cy.get("[data-cy=\"component_property-condition-setting_title\"]").click();
+    let targetDropBoxCy = "[data-cy=\"component_property-condition_use_javascript-autocomplete\"]";
+    cy.selectValueFromDropdownList(targetDropBoxCy, 3, "test-a");
+    cy.enterInputOrOutputFile(TYPE_OUTPUT, "testOutputFile", true, true);
+    cy.createComponent(DEF_COMPONENT_VIEWER, VIEWER_NAME_0, 300, 600);
+    cy.connectComponent(VIEWER_NAME_0); //コンポーネント同士を接続
+    cy.get("[data-cy=\"workflow-play-btn\"]").click(); //実行する
+    cy.checkProjectStatus("finished", 10000);
+    cy.removeAllProjects();
+
+    cy.visit("/");
+    cy.createProject(PROJECT_NAME, PROJECT_DESCRIPTION);
+    cy.projectOpen(PROJECT_NAME);
+
+    cy.get("[data-cy=\"workflow-open_viewer_screen-btn\"]").should("not.be.visible");
+    cy.get("[data-cy=\"workflow-viewer_screen-dialog\"]").should("not.exist");
+  });
 });
