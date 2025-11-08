e854fdd4ef2e4caa812535d58127deaba1ab4f37
Naoyuki Sogo <sogo@longtail-software.co.jp>
2025-11-09 00:58:59 +0900

    add dialog version of remotehost editor

--- client/src/components/Remotehost.vue
+++ client/src/components/Remotehost.vue
@@ -15,39 +15,9 @@
       @nav-icon-click="drawer=!drawer"
     />
     <v-main>
-      <v-toolbar
-        color="background"
-      >
-        <v-btn
-          text="new remote host setting"
-          data-cy="remotehost-new_remote_host_setting-btn"
-          @click.stop="openEditDialog()"
-        />
-      </v-toolbar>
-      <v-data-table
-        :items="hosts"
-        :headers="headers"
-        data-cy="remotehost-items-data_table"
-      >
-        <template #item.connectionTest="{ item, index }">
-          <v-btn
-            :disable="testing !== null && testing !== index"
-            :color="item.testResult"
-            :loading="item.loading"
-            :text="item.connectionStatus"
-            :prepend-icon="item.icon"
-            data-cy="remotehost-test-btn"
-            @click="testConnection(index)"
-          />
-        </template>
-        <template #item.action="{ item}">
-          <action-row
-            :item="item"
-            @delete="openRemoveConfirmDialog(item)"
-            @edit="openEditDialog(item)"
-          />
-        </template>
-      </v-data-table>
+      <remotehost-manager
+        :show-snackbar-func="showSnackbar"
+      />
       <v-snackbar
         v-model="openSnackbar"
         multi-line
@@ -65,27 +35,6 @@
           />
         </template>
       </v-snackbar>
-      <password-dialog
-        v-model="pwDialog"
-        :hostname="pwHostname"
-        :mode="pwMode"
-        @password="pwCallback"
-        @cancel="pwCallback(null)"
-      />
-      <remove-confirm-dialog
-        v-model="rmDialog"
-        :title="removeConfirmMessage"
-        @remove="removeRemotehost"
-      />
-      <add-new-host-dialog
-        v-model="newHostDialog"
-        max-width="80vw"
-        :initial-value="currentSetting"
-        :host-names="hostList"
-        :available-job-schedulers="jobSchedulerNames"
-        @new-host="addNewSetting"
-        @cancel="currentSetting={}"
-      />
     </v-main>
   </v-app>
 </template>
@@ -96,46 +45,20 @@ import Debug from "debug";
 const debug = Debug("wheel:remotehost");
 import SIO from "../lib/socketIOWrapper.js";
 import { readCookie } from "../lib/utility.js";
-import actionRow from "../components/common/actionRow.vue";
 import navDrawer from "../components/common/NavigationDrawer.vue";
-import removeConfirmDialog from "../components/common/removeConfirmDialog.vue";
-import passwordDialog from "../components/common/passwordDialog.vue";
-import addNewHostDialog from "../components/remotehost/addNewHostDialog.vue";
 import applicationToolBar from "../components/common/applicationToolBar.vue";
+import remotehostManager from "../components/remotehost/remotehostManager.vue";
 
 export default {
   name: "Remotehost",
   components: {
     navDrawer,
     applicationToolBar,
-    actionRow,
-    passwordDialog,
-    removeConfirmDialog,
-    addNewHostDialog
+    remotehostManager
   },
   data: ()=>{
     return {
-      drawer: false,
-      pwDialog: false,
-      pwCallback: null,
-      pwHostname: null,
-      pwMode: null,
-      rmDialog: false,
-      newHostDialog: false,
-      headers: [
-        { title: "name", key: "name" },
-        { title: "connection test", key: "connectionTest" },
-        { title: "hostname", key: "host" },
-        { title: "user", key: "user" },
-        { title: "port", key: "port" },
-        { title: "private key", key: "keyFile" },
-        { title: "action", key: "action", sortable: false }
-      ],
-      hosts: [],
-      jobSchedulerNames: [],
-      removeConfirmMessage: "",
-      currentSetting: {},
-      testing: null
+      drawer: false
     };
   },
   computed: {
@@ -143,147 +66,18 @@ export default {
       "openSnackbar",
       "snackbarMessage",
       "snackbarTimeout"
-    ]),
-    hostList() {
-      return this.hosts.map((host)=>{
-        return host.name;
-      }).filter((hostname)=>{
-        return hostname !== this.currentSetting.name;
-      });
-    }
+    ])
   },
-  mounted() {
+  beforeMount() {
     const baseURL = readCookie("socketIOPath");
     debug(`beseURL=${baseURL}`);
     SIO.init(null, baseURL);
-    SIO.emitGlobal("getJobSchedulerLabelList", (data)=>{
-      this.jobSchedulerNames.splice(0, this.jobSchedulerNames.length, ...data);
-    });
-    SIO.emitGlobal("getHostList", (data)=>{
-      data.forEach((e)=>{
-        e.icon = "mdi-lan-pending";
-        e.connectionStatus = "test";
-        e.testResult = "background";
-      });
-      this.hosts.splice(0, this.hosts.length, ...data);
-    });
-    SIO.onGlobal("askPassword", (hostname, mode, jwtServerURL, cb)=>{
-      this.pwCallback = (pw)=>{
-        cb(pw);
-      };
-
-      this.pwMode = mode;
-      this.pwHostname = hostname;
-      this.pwDialog = true;
-    });
-    SIO.onGlobal("logERR", (message)=>{
-      const rt = /^\[.*ERROR\].*- *(.*?)$/m.exec(message);
-      const output = rt ? rt[1] || rt[0] : message;
-      this.showSnackbar(output);
-    });
   },
   methods: {
     ...mapActions({
       showSnackbar: "showSnackbar",
       closeSnackbar: "closeSnackbar"
-    }),
-    openEditDialog(item) {
-      this.currentSetting = item || {};
-      this.newHostDialog = true;
-    },
-    initializeConnectionTestIcon(item) {
-      item.loading = false;
-      delete (item.testResult);
-      item.icon = "mdi-lan-pending";
-      item.connectionStatus = "test";
-      item.testResult = "background";
-    },
-    addNewSetting(updated) {
-      this.currentSetting = {};
-      delete (updated.icon);
-      delete (updated.connectionStatus);
-      delete (updated.testResult);
-      delete (updated.loading);
-
-      const eventName = updated.id ? "updateHost" : "addHost";
-      const index = updated.id
-        ? this.hosts.findIndex((e)=>{
-            return updated.id === e.id;
-          })
-        : 0;
-      const numDelete = updated.id ? 1 : 0;
-      SIO.emitGlobal(eventName, updated, (id)=>{
-        if (!id) {
-          console.log(`${eventName} API failed`, id);
-          return;
-        }
-        updated.id = id;
-        this.initializeConnectionTestIcon(updated);
-        this.hosts.splice(index, numDelete, updated);
-      });
-    },
-    openRemoveConfirmDialog(item) {
-      this.rmTarget = item;
-      this.removeConfirmMessage = `Are you shure you want to remove ${item.name} ?`;
-      this.rmDialog = true;
-    },
-    removeRemotehost() {
-      SIO.emitGlobal("removeHost", this.rmTarget.id, (rt)=>{
-        if (!rt) {
-          console.log("removeHost API failed", this.rmTarget.id);
-          return;
-        }
-        const index = this.hosts.findIndex((e)=>{
-          return e.id === this.rmTarget.id;
-        });
-        if (index >= 0) {
-          this.hosts.splice(index, 1);
-        }
-      });
-    },
-    testConnection(index) {
-      if (this.testing === index) {
-        debug(`ssh test for ${this.hosts[index].name} is already running`);
-        return;
-      }
-      if (this.testing !== null) {
-        this.showSnackbar({ message: "another ssh test is running", timeout: 1000 });
-        return;
-      }
-      this.testing = index;
-      const target = this.hosts[index];
-      target.loading = true;
-      SIO.emitGlobal("tryToConnect", this.hosts[index], (rt)=>{
-        debug("connection test result:", rt);
-        target.loading = false;
-
-        if (rt === "canceled") {
-          target.connectionStatus = "test";
-          target.icon = "mdi-lan-pending";
-          target.testResult = "background";
-        } else if (rt === "success") {
-          target.connectionStatus = "OK";
-          target.icon = "mdi-lan-connect";
-          target.testResult = "success";
-        } else {
-          target.connectionStatus = "failed";
-          target.icon = "mdi-lan-disconnect";
-          target.testResult = "error";
-
-          if (rt.code === "HostKeyError") {
-            if (rt.lineNumber && rt.host) {
-              this.snackbarMessage = `remote host identification of ${rt.host} is different from the one on line ${rt.lineNumber} of ~/.ssh/known_hosts`;
-            } else {
-              this.snackbarMessage = "remote host identification is different from the one stored in ~/.ssh/known_hosts";
-            }
-            this.openSnackbar = true;
-          }
-        }
-
-        this.pwDialog = false;
-        this.testing = null;
-      });
-    }
+    })
   }
 };
 </script>
