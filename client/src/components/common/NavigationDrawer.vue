ccad923d959bb546279861803664566f508e699b
Naoyuki Sogo <sogo@longtail-software.co.jp>
2025-11-24 00:52:37 +0900

    show modal version of rmeotehost when remotehost editor link clicked

--- client/src/components/common/NavigationDrawer.vue
+++ client/src/components/common/NavigationDrawer.vue
@@ -11,13 +11,13 @@
   >
     <v-list>
       <v-btn
+        v-if="!hideRemotehostButton"
         prepend-icon="mdi-cog-outline"
-        :href="remotehostURL"
-        target="_blank"
         variant="text"
         class="text-capitalize"
-        text="Remotehost editor"
-        data-cy="navigation-remote_host_editor-btn"
+        text="Manage remotehost"
+        data-cy="navigation-manage_remote_host-btn"
+        @click="$emit('open-remotehost-manager')"
       />
       <v-btn
         prepend-icon="mdi-help-circle-outline"
@@ -41,18 +41,20 @@ export default {
     baseUrl: {
       type: String,
       default: "."
+    },
+    hideRemotehostButton: {
+      type: Boolean,
+      default: false
     }
   },
   emits: [
-    "update:modelValue"
+    "update:modelValue",
+    "open-remotehost-manager"
   ],
   computed: {
     ...mapState(["readOnly"]),
     readOnlyColor() {
       return state2color(`${this.readOnly ? "paused" : ""}`);
-    },
-    remotehostURL() {
-      return `${this.baseUrl}/remotehost`;
     },
     drawer: {
       get() {
