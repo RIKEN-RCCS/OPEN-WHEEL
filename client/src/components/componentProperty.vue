a65fa594152ce2843bd6dd994fa040ad09898ed5
Naoyuki Sogo <sogo@longtail-software.co.jp>
2025-12-10 14:06:53 +0900

    now input files can be used as script and conditional script

--- client/src/components/componentProperty.vue
+++ client/src/components/componentProperty.vue
@@ -846,7 +846,22 @@ export default {
               .map((e)=>{
                 return e.name;
               });
-            this.commitScriptCandidates(scriptCandidates);
+            
+            // Add inputFiles (filtered for non-glob expressions)
+            const inputFileCandidates = this.selectedComponent?.inputFiles 
+              ? this.selectedComponent.inputFiles
+                  .map((file)=>{
+                    return typeof file === "string" ? file : file.name;
+                  })
+                  .filter((name)=>{
+                    // Filter out glob expressions (*, ?, [, ])
+                    return name && !name.match(/[*?\[\]]/);
+                  })
+              : [];
+            
+            // Merge and deduplicate
+            const allCandidates = [...new Set([...scriptCandidates, ...inputFileCandidates])];
+            this.commitScriptCandidates(allCandidates);
           }
           if (typeof this.selectedComponent.condition === "string") {
             this.conditionCheckByJS = !this.scriptCandidates.includes(this.selectedComponent.condition);
