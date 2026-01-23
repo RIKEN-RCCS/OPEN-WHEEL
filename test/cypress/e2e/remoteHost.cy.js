f975aaecc13f1347435bea7a4d0a4602045972d7
Keita Ota <k-ota@exchange.nes.sec.co.jp>
2026-01-23 21:12:51 +0900

    test: apply new naming to remote host

--- test/cypress/e2e/remoteHost.cy.js
+++ test/cypress/e2e/remoteHost.cy.js
@@ -1,4 +1,8 @@
-describe("01:リモートホスト画面基本動作確認", ()=>{
+/**
+ * リモートホスト画面テスト
+ */
+
+describe("remote host", ()=>{
   const LABEL = Math.random().toString(36)
     .substring(2, 10);
   const HOST_NAME = "TestHostName";
@@ -34,7 +38,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   リモートホスト設定画面への遷移
   試験確認内容：リモートホスト設定画面に遷移することを確認
    */
-  it("01-01-001:リモートホスト設定画面に遷移することを確認", ()=>{
+  it("リモートホスト設定画面に遷移することを確認", ()=>{
     cy.goToScreen("home");
     cy.get("[data-cy=\"tool_bar-navi-icon\"]").click();
     cy.get("[data-cy=\"navigation-remote_host_editor-btn\"]").invoke("removeAttr", "target")
@@ -47,7 +51,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   タイトル(WHEEL) ボタン押下
   試験確認内容：リモートホスト設定画面に遷移することを確認
    */
-  it("01-01-002:構成要素の機能動作確認-ホーム画面が表示されていることを確認", ()=>{
+  it("構成要素の機能動作確認-ホーム画面が表示されていることを確認", ()=>{
     cy.get("[data-cy=\"tool_bar-wheel_logo-logo\"]").click();
     cy.get("[data-cy=\"home-open-btn\"]").should("be.visible");
   });
@@ -57,7 +61,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「NEW REMOTE HOST SETTINGS」ボタン押下
   試験確認内容：リモートホスト設定作成ダイアログが表示されていることを確認
    */
-  it("01-01-003:構成要素の機能動作確認-「NEW REMOTE HOST SETTINGS」ボタン押下-リモートホスト設定作成ダイアログが表示されていることを確認", ()=>{
+  it("構成要素の機能動作確認-「NEW REMOTE HOST SETTINGS」ボタン押下-リモートホスト設定作成ダイアログが表示されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.get("[data-cy=\"add_new_host-add_new_host-card_title\"]").should("be.visible");
   });
@@ -67,7 +71,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「編集」ボタン押下
   試験確認内容：リモートホスト設定作成ダイアログが表示されていることを確認
    */
-  it("01-01-006:構成要素の機能動作確認-「編集」ボタン押下-リモートホスト設定作成ダイアログが表示されていることを確認", ()=>{
+  it("構成要素の機能動作確認-「編集」ボタン押下-リモートホスト設定作成ダイアログが表示されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.get("[data-cy=\"add_new_host-label-text_field\"]").type(LABEL);
     cy.get("[data-cy=\"add_new_host-hostname-text_field\"]").type(HOST_NAME);
@@ -94,7 +98,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   削除キャンセル
   試験確認内容：該当データの削除が行われていないことを確認
    */
-  it("01-01-007:構成要素の機能動作確認-「削除」ボタン押下-削除キャンセル-該当データの削除が行われていないことを確認", ()=>{
+  it("構成要素の機能動作確認-「削除」ボタン押下-削除キャンセル-該当データの削除が行われていないことを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.get("[data-cy=\"add_new_host-label-text_field\"]").type(LABEL);
     cy.get("[data-cy=\"add_new_host-hostname-text_field\"]").type(HOST_NAME);
@@ -119,7 +123,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   削除実行
   試験確認内容：該当データの削除が行われていることを確認
    */
-  it("01-01-008:構成要素の機能動作確認-「削除」ボタン押下-削除実行-該当データの削除が行われていることを確認", ()=>{
+  it("構成要素の機能動作確認-「削除」ボタン押下-削除実行-該当データの削除が行われていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.get("[data-cy=\"add_new_host-label-text_field\"]").type(LABEL);
     cy.get("[data-cy=\"add_new_host-hostname-text_field\"]").type(HOST_NAME);
@@ -147,7 +151,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「label」テキストエリア
   試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-009:構成要素の設定入力確認-テキスト入力-「label」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「label」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.get("[data-cy=\"add_new_host-label-text_field\"]").type(LABEL);
     cy.get("[data-cy=\"add_new_host-label-text_field\"]").find("input")
@@ -160,7 +164,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
    「Hostname」テキストエリア
    試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-010:構成要素の設定入力確認-テキスト入力-「Hostname」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「Hostname」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.get("[data-cy=\"add_new_host-hostname-text_field\"]").find("input")
@@ -173,7 +177,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「Port number」テキストエリア
   試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-011:構成要素の設定入力確認-テキスト入力-「Port number」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「Port number」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.get("[data-cy=\"add_new_host-port_number_label-text_field\"]").find("input")
@@ -186,7 +190,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「User ID」テキストエリア
   試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-012:構成要素の設定入力確認-テキスト入力-「User ID」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「User ID」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.get("[data-cy=\"add_new_host-user_id-text_field\"]").find("input")
@@ -199,7 +203,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「Host work dir」テキストエリア
   試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-013:構成要素の設定入力確認-テキスト入力-「Host work dir」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「Host work dir」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
@@ -213,7 +217,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「private key path」テキストエリア
   試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-014:構成要素の設定入力確認-テキスト入力-「private key path」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「private key path」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
@@ -227,7 +231,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「private key path」テキストエリア
   試験確認内容：選択したファイルのパスが正しく表示されていることを確認
    */
-  it("01-01-015:構成要素の設定入力確認-ファイル選択-「private key path」テキストエリア-選択したファイルのパスが正しく表示されていることを確認", ()=>{
+  it("構成要素の設定入力確認-ファイル選択-「private key path」テキストエリア-選択したファイルのパスが正しく表示されていることを確認", ()=>{
     cy.createProject("testProject", "testDescription");
     cy.goToScreen("remotehost");
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
@@ -248,7 +252,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「job scheduler」セレクトボックス
   試験確認内容：選択した値が正しく反映されていることを確認
    */
-  it("01-01-016:構成要素の設定入力確認-テキスト入力-「job scheduler」テキストエリア-選択した値が正しく反映されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「job scheduler」テキストエリア-選択した値が正しく反映されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
@@ -263,7 +267,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「max number of jobs」テキストエリア
   試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-017:構成要素の設定入力確認-テキスト入力-「max number of jobs」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「max number of jobs」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.get("[data-cy=\"add_new_host-max_number_of_jobs-text_field\"]").type(MAX_NUMBER);
@@ -277,7 +281,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「available queues」テキストエリア
   試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-018:構成要素の設定入力確認-テキスト入力-「available queues」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「available queues」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
@@ -291,7 +295,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「use bulkjob」チェックボックス
   試験確認内容：正しくチェックが行われていることを確認
    */
-  it("01-01-019:構成要素の設定入力確認-テキスト入力-「use bulkjob」チェックボックス-正しくチェックが行われていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「use bulkjob」チェックボックス-正しくチェックが行われていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
@@ -309,7 +313,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「use stepjob」チェックボックス
   試験確認内容：正しくチェックが行われていることを確認
    */
-  it("01-01-020:構成要素の設定入力確認-テキスト入力-「use stepjob」チェックボックス-正しくチェックが行われていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「use stepjob」チェックボックス-正しくチェックが行われていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
@@ -327,7 +331,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「shared host」セレクトボックス
   試験確認内容：選択した値が正しく反映されていることを確認
    */
-  it("01-01-021:構成要素の設定入力確認-テキスト入力-「shared host」セレクトボックス-選択した値が正しく反映されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「shared host」セレクトボックス-選択した値が正しく反映されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.get("[data-cy=\"add_new_host-ok-btn\"]").should("not.be.disabled")
@@ -346,7 +350,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「shared path on shared host」テキストエリア
   試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-022:構成要素の設定入力確認-テキスト入力-「shared path on shared host」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「shared path on shared host」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
@@ -360,7 +364,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「connection renewal interval (min.)」テキストエリア
   試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-023:構成要素の設定入力確認-テキスト入力-「connection renewal interval (min.)」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「connection renewal interval (min.)」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
@@ -375,7 +379,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「status check interval (sec.)」テキストエリア
   試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-024:構成要素の設定入力確認-テキスト入力-「status check interval (sec.)」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「status check interval (sec.)」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
@@ -390,7 +394,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「max number of status check error allowed」テキストエリア
   試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-025:構成要素の設定入力確認-テキスト入力-「max number of status check error allowed」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「max number of status check error allowed」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
@@ -405,7 +409,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「execution interval」テキストエリア
   試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-026:構成要素の設定入力確認-テキスト入力-「execution interval」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「execution interval」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
@@ -420,7 +424,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   「timeout during handshake phase」テキストエリア
   試験確認内容：テキストが正しく入力されていることを確認
    */
-  it("01-01-027:構成要素の設定入力確認-テキスト入力-「timeout during handshake phase」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
+  it("構成要素の設定入力確認-テキスト入力-「timeout during handshake phase」テキストエリア-テキストが正しく入力されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
@@ -434,7 +438,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   一覧から確認
   試験確認内容：値が正しく反映されていることを確認
    */
-  it("01-01-028:設定入力後の反映確認-一覧から確認-値が正しく反映されていることを確認", ()=>{
+  it("設定入力後の反映確認-一覧から確認-値が正しく反映されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_NO, STEPJOB_CHK_NO, SHARED_HOST);
@@ -453,7 +457,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   基本設定確認
   試験確認内容：値が正しく反映されていることを確認
    */
-  it("01-01-029:設定入力後の反映確認-編集ダイアログから確認-基本設定確認-値が正しく反映されていることを確認", ()=>{
+  it("設定入力後の反映確認-編集ダイアログから確認-基本設定確認-値が正しく反映されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
@@ -496,7 +500,7 @@ describe("01:リモートホスト画面基本動作確認", ()=>{
   詳細設定確認
   試験確認内容：値が正しく反映されていることを確認
    */
-  it("01-01-030:設定入力後の反映確認-編集ダイアログから確認-詳細設定確認-値が正しく反映されていることを確認", ()=>{
+  it("設定入力後の反映確認-編集ダイアログから確認-詳細設定確認-値が正しく反映されていることを確認", ()=>{
     cy.get("[data-cy=\"remotehost-new_remote_host_setting-btn\"]").click();
     cy.enterRequiredRemoteHost(LABEL, HOST_NAME, PORT_NUMBER, TEST_USER);
     cy.enterRemoteHost(HOST_WORK_DIR, PRIVATE_KEY_FILE, JOB_SCHEDULERS, MAX_NUMBER, AVAILABLE_QUEUES, BULKJOB_CHK_YES, STEPJOB_CHK_YES, SHARED_HOST);
