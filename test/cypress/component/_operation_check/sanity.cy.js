956b330da25cebe3fd923b473f7b72c49cc58be5
miyamori yuji <miyamori-yuji-k@sec.co.jp>
2026-02-03 09:11:28 +0900

    test(cypress): del operation check code

--- test/cypress/component/_operation_check/sanity.cy.js
+++
@@ -1,7 +0,0 @@
-// test/cypress/component/sanity.cy.js
-import { mount } from 'cypress/vue'
-import { h } from 'vue'
-it('sanity', () => {
-  mount({ render: () => h('div', 'ok') })
-  cy.contains('ok')
-})
