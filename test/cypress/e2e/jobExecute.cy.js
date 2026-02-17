3b034383b0253c56f5975987674444eaa4285592
miyamori yuji <miyamori-yuji-k@sec.co.jp>
2026-02-17 16:40:42 +0900

    test(cypress): fix e2e test jobexecute jobExecute.cy.js
    リモートホスト向けのテストを追加
    リモートホスト設定を追加

--- test/cypress/e2e/jobExecute.cy.js
+++ test/cypress/e2e/jobExecute.cy.js
@@ -6,6 +6,9 @@ describe("jobExecute", ()=>{
   const TASK_NAME_1 = "task1";
   const DEF_COMPONENT_FOREACH = "foreach";
   const FOREACH_NAME_0 = "foreach0";
+  const HOST_NAME = "wheel_release_test_server";
+  const FILE_NAME_TASK1 = "task1.sh";
+  const FILE_NAME_TASK2 = "task2.sh";
 
   before(()=>{
     return cy.removeAllProjects();
@@ -20,15 +23,12 @@ describe("jobExecute", ()=>{
     return cy.removeAllProjects();
   });
 
-
   /**
    * localhostでのタスク実行
    * 現状実行完了までを確認
    * ファイルの生成の確認方法を検討中
    */
   it("localhostでのタスク実行", ()=>{
-    const fileNameTask1 = "task1.sh";
-    const fileNameTask2 = "task2.sh";
     const codeTask1 = `echo "test" > message.txt`;
     const codeTask2 = `cat message.txt >/dev/null 2>&1`;
 
@@ -47,14 +47,14 @@ describe("jobExecute", ()=>{
     //プロパティを閉じる
     cy.closeProperty();
 
-    //foreach作成
+    //foreach選択
     cy.doubleClickComponentName(FOREACH_NAME_0);
 
     //task 1つ目
     cy.createComponent(DEF_COMPONENT_TASK, TASK_NAME_0, 501, 500);
 
     //シェル作成
-    cy.setupTaskWithScriptAndIO(fileNameTask1, codeTask1, TYPE_OUTPUT, "message.txt", "localhost");
+    cy.setupTaskWithScriptAndIO(FILE_NAME_TASK1, codeTask1, TYPE_OUTPUT, "message.txt", "localhost");
 
     //プロパティを閉じる
     cy.closeProperty();
@@ -63,7 +63,7 @@ describe("jobExecute", ()=>{
     cy.createComponent(DEF_COMPONENT_TASK, TASK_NAME_1, 501, 700);
 
     //シェル作成
-    cy.setupTaskWithScriptAndIO(fileNameTask2, codeTask2, TYPE_INPUT, "message.txt", "localhost");
+    cy.setupTaskWithScriptAndIO(FILE_NAME_TASK2, codeTask2, TYPE_INPUT, "message.txt", "localhost");
 
     //プロパティを閉じる
     cy.closeProperty();
@@ -77,7 +77,57 @@ describe("jobExecute", ()=>{
     //タスク実行
     cy.get("[data-cy=\"workflow-play-btn\"]").click();
 
-    //完了まち
+    //完了まで待機
+    cy.checkProjectStatus("finished");
+  });
+
+  /**
+   * localhostでのタスク実行
+   * 現状実行完了までを確認
+   * ファイルの生成の確認方法を検討中
+   */
+  it("remoteHostでのタスク実行", ()=>{
+    const codeTask1 = `echo "test" > message.txt`;
+    const codeTask2 = `cat message.txt >/dev/null 2>&1`;
+
+    //task 1つ目
+    cy.createComponent(DEF_COMPONENT_TASK, TASK_NAME_0, 501, 500);
+
+    //シェル作成
+    cy.setupTaskWithScriptAndIO(FILE_NAME_TASK1, codeTask1, TYPE_OUTPUT, "message.txt", HOST_NAME);
+
+    //jobスケジューラ有効化
+    cy.switchUseJobScheduler("on");
+
+    //プロパティを閉じる
+    cy.closeProperty();
+
+    //task 2つ目
+    cy.createComponent(DEF_COMPONENT_TASK, TASK_NAME_1, 501, 700);
+
+    //シェル作成
+    cy.setupTaskWithScriptAndIO(FILE_NAME_TASK2, codeTask2, TYPE_INPUT, "message.txt", HOST_NAME);
+
+    //jobスケジューラ有効化
+    cy.switchUseJobScheduler("on");
+
+    //プロパティを閉じる
+    cy.closeProperty();
+
+    //コンポーネント同士を接続
+    cy.connectComponentMultiple(TASK_NAME_0, TASK_NAME_1);
+
+    //待機
+    cy.wait(300);
+
+    //タスク実行
+    cy.get("[data-cy=\"workflow-play-btn\"]").click();
+
+    //リモートアクセスパスワード
+    cy.wait(300);
+    cy.passwordType("passw0rd");
+
+    //完了まで待機
     cy.checkProjectStatus("finished");
   });
 });
