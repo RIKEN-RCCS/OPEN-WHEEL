88fb0cdb2510304f51fd798089e4fd679af7e45c
miyamori yuji <miyamori-yuji-k@sec.co.jp>
2026-03-17 13:45:17 +0900

    test(cypress): fix e2e test storage.cy.js
    components\storage.cy.js 不具合発生でskipしていた試験
    不具合解消が確認できたのでskip解除
    
    新規不具合を確認した試験にはskip設定したままコメントにその旨を追記

--- test/cypress/e2e/components/storage.cy.js
+++ test/cypress/e2e/components/storage.cy.js
@@ -291,9 +291,8 @@ describe("components", ()=>{
   ファイル操作エリア
   ディレクトリ単体表示
   試験確認内容：ディレクトリが単体表示されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.) task-aがすでにある状態で登録部分で重複警告が出ている
      */
-    it.skip("ファイル操作エリア-ディレクトリ単体表示-ディレクトリが単体表示されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
+    it("ファイル操作エリア-ディレクトリ単体表示-ディレクトリが単体表示されることを確認", ()=>{ 
       cy.createComponent(DEF_COMPONENT_STORAGE, STORAGE_NAME_0, 501, 500);
       cy.get("[data-cy=\"component_property-directory_path-text_field\"]").type(wheelPath);
       cy.createDirOrFile(TYPE_DIR, "test-a", true);
@@ -322,7 +321,7 @@ describe("components", ()=>{
   ファイル操作エリア
   ディレクトリ複数表示（リロード前）
   試験確認内容：ディレクトリが単体表示されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.)
+  不具合内容:Filesに指定のディレクトリがないため失敗　詳細調査中
      */
     it.skip("ファイル操作エリア-ディレクトリ複数表示（リロード前）-ディレクトリが単体表示されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
       cy.createComponent(DEF_COMPONENT_STORAGE, STORAGE_NAME_0, 501, 500);
@@ -355,7 +354,7 @@ describe("components", ()=>{
   ファイル操作エリア
   ディレクトリ複数表示（リロード後）
   試験確認内容：ディレクトリが複数表示されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.) task-aがすでにある状態で登録部分で重複警告が出ている
+  不具合内容:Filesに指定のディレクトリがないため失敗　詳細調査中
      */
     it.skip("ファイル操作エリア-ディレクトリ複数表示（リロード後）-ディレクトリが複数表示されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
       cy.createComponent(DEF_COMPONENT_STORAGE, STORAGE_NAME_0, 501, 500);
@@ -389,9 +388,8 @@ describe("components", ()=>{
   ファイル操作エリア
   ファイル単体表示
   試験確認内容：ファイルが単体表示されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.) task-aがすでにある状態で登録部分で重複警告が出ている
      */
-    it.skip("ファイル操作エリア-ファイル単体表示-ファイルが単体表示されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
+    it("ファイル操作エリア-ファイル単体表示-ファイルが単体表示されることを確認", ()=>{
       cy.createComponent(DEF_COMPONENT_STORAGE, STORAGE_NAME_0, 501, 500);
       cy.get("[data-cy=\"component_property-directory_path-text_field\"]").type(wheelPath);
       cy.createDirOrFile(TYPE_FILE, "test-a", true);
@@ -420,9 +418,8 @@ describe("components", ()=>{
   ファイル操作エリア
   ファイル複数表示（リロード前）
   試験確認内容：ファイルが単体表示されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.)
      */
-    it.skip("ファイル操作エリア-ファイル複数表示（リロード前）-ファイルが単体表示されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
+    it("ファイル操作エリア-ファイル複数表示（リロード前）-ファイルが単体表示されることを確認", ()=>{
       cy.createComponent(DEF_COMPONENT_STORAGE, STORAGE_NAME_0, 501, 500);
       cy.get("[data-cy=\"component_property-directory_path-text_field\"]").type(wheelPath);
       cy.createDirOrFile(TYPE_FILE, "test1", true);
@@ -453,7 +450,7 @@ describe("components", ()=>{
   ファイル操作エリア
   ファイル複数表示（リロード後）
   試験確認内容：ファイルが複数表示されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.)
+  不具合内容:Filesに指定のディレクトリがないため失敗　詳細調査中
      */
     it.skip("ファイル操作エリア-ファイル複数表示（リロード後）-ファイルが複数表示されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
       cy.createComponent(DEF_COMPONENT_STORAGE, STORAGE_NAME_0, 501, 500);
@@ -489,9 +486,8 @@ describe("components", ()=>{
   ファイル操作エリア
   ディレクトリ内ディレクトリ表示
   試験確認内容：ディレクトリ内にディレクトリが作成されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.) task-aがすでにある状態で登録部分で重複警告が出ている
      */
-    it.skip("ファイル操作エリア-ディレクトリ内ディレクトリ表示-ディレクトリ内にディレクトリが作成されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
+    it("ファイル操作エリア-ディレクトリ内ディレクトリ表示-ディレクトリ内にディレクトリが作成されることを確認", ()=>{ 
       cy.createComponent(DEF_COMPONENT_STORAGE, STORAGE_NAME_0, 501, 500);
       cy.get("[data-cy=\"component_property-directory_path-text_field\"]").type(wheelPath);
       cy.createDirOrFile(TYPE_DIR, "test-a", true);
@@ -521,9 +517,8 @@ describe("components", ()=>{
   ファイル操作エリア
   ディレクトリ内ファイル表示
   試験確認内容：ディレクトリ内にファイルが作成されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.) task-aがすでにある状態で登録部分で重複警告が出ている
      */
-    it.skip("ファイル操作エリア-ディレクトリ内ファイル表示-ディレクトリ内にファイルが作成されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
+    it("ファイル操作エリア-ディレクトリ内ファイル表示-ディレクトリ内にファイルが作成されることを確認", ()=>{
       cy.createComponent(DEF_COMPONENT_STORAGE, STORAGE_NAME_0, 501, 500);
       cy.get("[data-cy=\"component_property-directory_path-text_field\"]").type(wheelPath);
       cy.createDirOrFile(TYPE_DIR, "test-a", true);
