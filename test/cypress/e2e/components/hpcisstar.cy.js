9cb53770b3ff28dc922658039a1a88336abe49c5
miyamori yuji <miyamori-yuji-k@sec.co.jp>
2026-03-17 13:23:09 +0900

    test(cypress): fix e2e test hpcisstar.cy.js
    components\hpcisstar.cy.js 不具合発生でskipしていた試験
    不具合解消が確認できたのでskip解除

--- test/cypress/e2e/components/hpcisstar.cy.js
+++ test/cypress/e2e/components/hpcisstar.cy.js
@@ -293,9 +293,8 @@ describe("components", ()=>{
   ファイル操作エリア
   ディレクトリ単体表示
   試験確認内容：ディレクトリが単体表示されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.)
      */
-    it.skip("tarコンポーネント共通機能確認-ファイル操作エリア-ディレクトリ単体表示-ディレクトリが単体表示されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
+    it("tarコンポーネント共通機能確認-ファイル操作エリア-ディレクトリ単体表示-ディレクトリが単体表示されることを確認", ()=>{
       cy.createComponent(DEF_COMPONENT_HPCISS, HPCISS_NAME_0, 501, 500);
       cy.get("[data-cy=\"component_property-directory_path-text_field\"]").type(wheelPath);
       cy.createDirOrFile(TYPE_DIR, "test-a", true);
@@ -324,9 +323,8 @@ describe("components", ()=>{
   ファイル操作エリア
   ディレクトリ複数表示（リロード前）
   試験確認内容：ディレクトリが単体表示されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.)
      */
-    it.skip("tarコンポーネント共通機能確認-ファイル操作エリア-ディレクトリ複数表示（リロード前）-ディレクトリが単体表示されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
+    it("tarコンポーネント共通機能確認-ファイル操作エリア-ディレクトリ複数表示（リロード前）-ディレクトリが単体表示されることを確認", ()=>{ 
       cy.createComponent(DEF_COMPONENT_HPCISS, HPCISS_NAME_0, 501, 500);
       cy.get("[data-cy=\"component_property-directory_path-text_field\"]").type(wheelPath);
       cy.createDirOrFile(TYPE_DIR, "test1", true);
@@ -355,9 +353,8 @@ describe("components", ()=>{
   ファイル操作エリア
   ディレクトリ複数表示（リロード後）
   試験確認内容：ディレクトリが複数表示されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.)
      */
-    it.skip("tarコンポーネント共通機能確認-ファイル操作エリア-ディレクトリ複数表示（リロード後）-ディレクトリが複数表示されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
+    it("tarコンポーネント共通機能確認-ファイル操作エリア-ディレクトリ複数表示（リロード後）-ディレクトリが複数表示されることを確認", ()=>{ 
       cy.createComponent(DEF_COMPONENT_HPCISS, HPCISS_NAME_0, 501, 500);
       cy.get("[data-cy=\"component_property-directory_path-text_field\"]").type(wheelPath);
       cy.createDirOrFile(TYPE_DIR, "test1", true);
@@ -388,9 +385,8 @@ describe("components", ()=>{
   ファイル操作エリア
   ファイル単体表示
   試験確認内容：ファイルが単体表示されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.)
      */
-    it.skip("tarコンポーネント共通機能確認-ファイル操作エリア-ファイル単体表示-ファイルが単体表示されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
+    it("tarコンポーネント共通機能確認-ファイル操作エリア-ファイル単体表示-ファイルが単体表示されることを確認", ()=>{ 
       cy.createComponent(DEF_COMPONENT_HPCISS, HPCISS_NAME_0, 501, 500);
       cy.get("[data-cy=\"component_property-directory_path-text_field\"]").type(wheelPath);
       cy.createDirOrFile(TYPE_FILE, "test-a", true);
@@ -419,9 +415,8 @@ describe("components", ()=>{
   ファイル操作エリア
   ファイル複数表示（リロード前）
   試験確認内容：ファイルが単体表示されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.)
      */
-    it.skip("tarコンポーネント共通機能確認-ファイル操作エリア-ファイル複数表示（リロード前）-ファイルが単体表示されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
+    it.skip("tarコンポーネント共通機能確認-ファイル操作エリア-ファイル複数表示（リロード前）-ファイルが単体表示されることを確認", ()=>{
       cy.createComponent(DEF_COMPONENT_HPCISS, HPCISS_NAME_0, 501, 500);
       cy.get("[data-cy=\"component_property-directory_path-text_field\"]").type(wheelPath);
       cy.createDirOrFile(TYPE_FILE, "test1", true);
@@ -450,9 +445,8 @@ describe("components", ()=>{
   ファイル操作エリア
   ファイル複数表示（リロード後）
   試験確認内容：ファイルが複数表示されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.)
      */
-    it.skip("tarコンポーネント共通機能確認-ファイル操作エリア-ファイル複数表示（リロード後）-ファイルが複数表示されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
+    it.skip("tarコンポーネント共通機能確認-ファイル操作エリア-ファイル複数表示（リロード後）-ファイルが複数表示されることを確認", ()=>{ 
       cy.createComponent(DEF_COMPONENT_HPCISS, HPCISS_NAME_0, 501, 500);
       cy.get("[data-cy=\"component_property-directory_path-text_field\"]").type(wheelPath);
       cy.createDirOrFile(TYPE_FILE, "test1", true);
@@ -483,9 +477,8 @@ describe("components", ()=>{
   ファイル操作エリア
   ディレクトリ内ディレクトリ表示
   試験確認内容：ディレクトリ内にディレクトリが作成されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.)
      */
-    it.skip("tarコンポーネント共通機能確認-ファイル操作エリア-ディレクトリ内ディレクトリ表示-ディレクトリ内にディレクトリが作成されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
+    it.skip("tarコンポーネント共通機能確認-ファイル操作エリア-ディレクトリ内ディレクトリ表示-ディレクトリ内にディレクトリが作成されることを確認", ()=>{
       cy.createComponent(DEF_COMPONENT_HPCISS, HPCISS_NAME_0, 501, 500);
       cy.get("[data-cy=\"component_property-directory_path-text_field\"]").type(wheelPath);
       cy.createDirOrFile(TYPE_DIR, "test-a", true);
@@ -515,9 +508,8 @@ describe("components", ()=>{
   ファイル操作エリア
   ディレクトリ内ファイル表示
   試験確認内容：ディレクトリ内にファイルが作成されることを確認
-  不具合内容:file削除確認ダイアログのOK不備(1.)
      */
-    it.skip("tarコンポーネント共通機能確認-ファイル操作エリア-ディレクトリ内ファイル表示-ディレクトリ内にファイルが作成されることを確認", ()=>{ //TODO:テストで失敗しているため一時的にskip.修正後復帰すること.
+    it.skip("tarコンポーネント共通機能確認-ファイル操作エリア-ディレクトリ内ファイル表示-ディレクトリ内にファイルが作成されることを確認", ()=>{ 
       cy.createComponent(DEF_COMPONENT_HPCISS, HPCISS_NAME_0, 501, 500);
       cy.get("[data-cy=\"component_property-directory_path-text_field\"]").type(wheelPath);
       cy.createDirOrFile(TYPE_DIR, "test-a", true);
